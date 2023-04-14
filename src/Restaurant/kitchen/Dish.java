2175ab6aa76be6aab3b5c6cfa27af9e6275d78c0
kvit7 <kvit72v@yandex.ru>
2023-04-14 18:30:59 +0300

    В класс Order добавлен метод getTotalCookingTime и isEmpty. В классе Dish изменен добавлено время приготовления блюд и метод getDuration.

--- src/Restaurant/kitchen/Dish.java
+++ src/Restaurant/kitchen/Dish.java
@@ -1,11 +1,21 @@
 package Restaurant.kitchen;
 
 public enum Dish {
-    FISH,
-    STEAK,
-    SOUP,
-    JUICE,
-    WATER;
+    FISH(25),
+    STEAK(30),
+    SOUP(15),
+    JUICE(5),
+    WATER(3);
+
+    private int duration;
+
+    public int getDuration() {
+        return duration;
+    }
+
+    Dish(int duration) {
+        this.duration = duration;
+    }
 
     public static String allDishesToString() {
         String result = "";
