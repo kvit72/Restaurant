4b77d7d4c44e87a248f6774b273ee105f668a402
kvit7 <kvit72v@yandex.ru>
2023-04-17 10:27:32 +0300

    В классе AdvertisementStorage добавлены методы для работы с видео.

--- src/Restaurant/ad/AdvertisementStorage.java
+++ src/Restaurant/ad/AdvertisementStorage.java
@@ -1,5 +1,8 @@
 package Restaurant.ad;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class AdvertisementStorage {
     private static class InstanceHolder {
         private static final AdvertisementStorage ourInstance = new AdvertisementStorage();
@@ -9,7 +12,20 @@ public class AdvertisementStorage {
         return InstanceHolder.ourInstance;
     }
 
-    private AdvertisementStorage() {
+    private final List<Advertisement> videos = new LinkedList<>();
 
+    private AdvertisementStorage() {
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60));
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60));
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));
+    }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement)
     }
 }
