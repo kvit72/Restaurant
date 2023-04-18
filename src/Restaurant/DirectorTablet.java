b5668863b8ebc80939d9b84175e572aad8f365a5
kvit7 <kvit72v@yandex.ru>
2023-04-18 15:03:52 +0300

    Создан класс StatisticAdvertisementManager. В классе DirectorTablet реализованы методы printArchivedVideoSet, printActiveVideoSet.

--- src/Restaurant/DirectorTablet.java
+++ src/Restaurant/DirectorTablet.java
@@ -1,11 +1,10 @@
 package Restaurant;
 
+import Restaurant.ad.Advertisement;
+import Restaurant.ad.StatisticAdvertisementManager;
 import Restaurant.statistic.StatisticManager;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 public class DirectorTablet {
     public void printAdvertisementProfit() {
@@ -38,15 +37,33 @@ public class DirectorTablet {
 
             System.out.println();
         }
-
-
     }
 
     public void printActiveVideoSet() {
+        List<Advertisement> videoSet = StatisticAdvertisementManager.getInstance().getVideoSet(true);
+        Collections.sort(videoSet, new Comparator<Advertisement>() {
+            @Override
+            public int compare(Advertisement o1, Advertisement o2) {
+                return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+            }
+        });
 
+        for (Advertisement advertisement : videoSet) {
+            System.out.println(advertisement.getName() + " - " + advertisement.getHits());
+        }
     }
 
     public void printArchivedVideoSet() {
+        List<Advertisement> videoSet = StatisticAdvertisementManager.getInstance().getVideoSet(false);
+        Collections.sort(videoSet, new Comparator<Advertisement>() {
+            @Override
+            public int compare(Advertisement o1, Advertisement o2) {
+                return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+            }
+        });
 
+        for (Advertisement advertisement : videoSet) {
+            System.out.println(advertisement.getName());
+        }
     }
 }
