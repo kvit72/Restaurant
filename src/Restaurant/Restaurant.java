a5ecee7ce6aaa9c5f8ea42c222380bafff7a70e2
kvit7 <kvit72v@yandex.ru>
2023-04-18 16:04:44 +0300

    Рефакторинг метода main класса Restaurant.

--- src/Restaurant/Restaurant.java
+++ src/Restaurant/Restaurant.java
@@ -2,15 +2,41 @@ package Restaurant;
 
 import Restaurant.kitchen.Cook;
 import Restaurant.kitchen.Waiter;
+import Restaurant.statistic.StatisticManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Restaurant {
     private static final int ORDER_CREATING_INTERVAL = 100;
     public static void main(String[] args) {
-        Tablet tablet = new Tablet(5);
-        Cook cook = new Cook("Dima");
-        Waiter waiter = new Waiter();
-        tablet.addObserver(cook);
-        cook.addObserver(waiter);
+        List<Tablet> tablets = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            tablets.add(new Tablet(i + 1));
+        }
+
+        Cook cook1 = new Cook("Dima");
+        Cook cook2 = new Cook("Dima");
+
+        StatisticManager.getInstance().register(cook1);
+        StatisticManager.getInstance().register(cook2);
+
+        for (Tablet tablet :
+                tablets) {
+            tablet.addObserver(cook1);
+            tablet.addObserver(cook2);
+        }
+
+        Thread thread = new Thread(new RandomOrderGeneratorTask(tablets, ORDER_CREATING_INTERVAL));
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+            thread.interrupt();
+            thread.join();
+            Thread.sleep(1000);
+        } catch (InterruptedException ignore) {
+        }
 
         DirectorTablet directorTablet = new DirectorTablet();
         directorTablet.printAdvertisementProfit();
