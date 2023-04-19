74c4bbc4491eef509343078ee7d4457e18cc408a
kvit7 <kvit72v@yandex.ru>
2023-04-19 11:50:15 +0300

    В классе Cook создано поле busy и геттер к нему. В методе startCookingOrder класса Cook вызван метод Thread.sleep. В классе OrderManager рпеализован метод run.

--- src/Restaurant/OrderManager.java
+++ src/Restaurant/OrderManager.java
@@ -1,9 +1,12 @@
 package Restaurant;
 
+import Restaurant.kitchen.Cook;
 import Restaurant.kitchen.Order;
+import Restaurant.statistic.StatisticManager;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class OrderManager implements Observer {
@@ -12,6 +15,21 @@ public class OrderManager implements Observer {
     public OrderManager() {
         Thread thread = new Thread() {
             public void run() {
+
+                Set<Cook> cooks = StatisticManager.getInstance().getCooks();
+                try {
+                    while (true) {
+                        Thread.sleep(10);
+                        if (!orderQueue.isEmpty()) {
+                            for (Cook cook : cooks) {
+                                if (!cook.isBusy()) {
+                                    cook.startCookingOrder(orderQueue.take());
+                                }
+                            }
+                        }
+                    }
+                } catch (InterruptedException e) {
+                }
             }
         };
         thread.setDaemon(true);
--- src/Restaurant/kitchen/Cook.java
+++ src/Restaurant/kitchen/Cook.java
@@ -1,6 +1,7 @@
 package Restaurant.kitchen;
 
 import Restaurant.ConsoleHelper;
+import Restaurant.Tablet;
 import Restaurant.statistic.StatisticManager;
 import Restaurant.statistic.event.CookedOrderEventDataRow;
 
@@ -10,16 +11,35 @@ import java.util.Observer;
 public class Cook extends Observable {
     private final String name;
 
+    private boolean busy;
+
     public Cook(String name) {
         this.name = name;
     }
 
+    public boolean isBusy() {
+        return busy;
+    }
+
     public void startCookingOrder(Order order) {
-        ConsoleHelper.writeMessage("Start cooking - " + order);
+        this.busy = true;
+
+        Tablet tablet = order.getTablet();
+
+        ConsoleHelper.writeMessage(name + "Start cooking - " + order);
+
+        int totalCookingTime = order.getTotalCookingTime();
+        CookedOrderEventDataRow row = new CookedOrderEventDataRow(order.getTablet().toString(), name, totalCookingTime * 60, order.getDishes());
+        StatisticManager.getInstance().register(row);
+
+        try {
+            Thread.sleep(totalCookingTime * 10);
+        } catch (InterruptedException ignored) {
+        }
+
         setChanged();
         notifyObservers(order);
-        CookedOrderEventDataRow row = new CookedOrderEventDataRow(order.getTablet().toString(), name, order.getTotalCookingTime() * 60, order.getDishes());
-        StatisticManager.getInstance().register(row);
+        this.busy = false;
     }
 
     @Override
